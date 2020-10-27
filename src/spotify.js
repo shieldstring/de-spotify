74440e7703beb31b8bbac311ee06f79c94338ced
shieldstring <edmunddaniels06@gmail.com>
2020-10-27 16:45:52 +0100

    finially deployed with firebase

--- src/spotify.js
+++ src/spotify.js
@@ -11,6 +11,9 @@ const scopes = [
     "user-read-playback-state",
     "user-top-read",
     "user-modify-playback-state",
+    "streaming",
+    "user-read-email", 
+    "user-read-private",
 ];
 
 export const getTokenFromUrl = () => {
