b8afdad27bead5d71fe809a58bb73c5007a48742
shieldstring <edmunddaniels06@gmail.com>
2020-10-27 15:10:43 +0100

    geting ready to be deployed

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,7 @@ export const initialState = {
     playlist: [],
     playing: false,
     item: null,
-    // remove after finished developing...
-    token: "BQALNyJ5hGTwaNb4Vi1gwrHWIED4aalgmlvjz_kuls6-efuxAj7XrXTMC_wqN1JhNgu3041JrRNVKWsC3ufnUP61GcMJQ6HNJUtFATgxjWLCalpOkhUtpRbr0XwxVtR3o1p5bSE-sx1-CVAd0keNZHzflZGQ_flX",
+    token: "",
 };
 
 const reducer = (state, action) =>{
